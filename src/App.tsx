b6c534a7dc8daa0e623add085e7ee5e7c951413c
최동민 <dm.choi@logiall.com>
2024-03-17 01:07:23 +0900

    stackflow 기본 세팅

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "@stackflow/plugin-basic-ui/index.css";
+
+import "./App.css";
+import { Stack } from "./stackflow";
+
+function App() {
+  return (
+    <div>
+      <Stack />
+    </div>
+  );
+}
+
+export default App;
